6bf7ef5c25844b58e1133f624c8d86e7003810d2
Eugene Soo <20952179+eugenesoo@users.noreply.github.com>
2020-06-14 14:19:48 +0800

    Fix small fire particle for purifier I changed the ability to not be hidden for easier debugging
    Signed-off-by: Eugene Soo <20952179+eugenesoo@users.noreply.github.com>

--- scripts/vscripts/spells/small_fire.lua
+++ scripts/vscripts/spells/small_fire.lua
@@ -1,6 +1,5 @@
 function CreateSmallFire( keys )
-    print(keys)
     local caster = keys.caster
-    local fireParticle = ParticleManager:CreateParticleForPlayer("models/items/queenofpain/queenofpain_arcana/debut/particles/lava/lava_bubbling_ambient.vpcf", PATTACH_ABSORIGIN_FOLLOW, purifier, player)
+    local fireParticle = ParticleManager:CreateParticle("particles/dire_fx/fire_barracks.vpcf", PATTACH_ABSORIGIN_FOLLOW, caster)
     ParticleManager:SetParticleControl(fireParticle, 1, caster:GetAbsOrigin())
 end
