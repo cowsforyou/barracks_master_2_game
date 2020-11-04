76aeda30df2ec9303311bf4b99f723c8f9c44ed3
Eugene Soo <20952179+eugenesoo@users.noreply.github.com>
2020-11-04 20:19:14 +0800

    Fix Rage Sacrifice
    Signed-off-by: Eugene Soo <20952179+eugenesoo@users.noreply.github.com>

--- scripts/vscripts/spells/xoo_rage_sacrifice.lua
+++ scripts/vscripts/spells/xoo_rage_sacrifice.lua
@@ -1,12 +1,12 @@
 xoo_rage_sacrifice = class({})
 
 --------------------------------------------------------------------------------
-function modifier_xoo_rage_sacrifice_drain(keys)
-	local new_hp = keys.caster:GetHealth() - (keys.UnholyHealthDrainPerSecond * keys.UnholyHealthDrainInterval)
+function Sacrifice(keys)
+	local new_hp = keys.target:GetHealth() * 0.8
 	
-	if new_hp < 1 then  --Armlet cannot kill the caster from its HP drain.
+	if new_hp < 1 then  --Cannot kill the caster from its HP drain.
 		new_hp = 1
 	end
 	
-	keys.caster:SetHealth(new_hp)
+	keys.target:SetHealth(new_hp)
 end
