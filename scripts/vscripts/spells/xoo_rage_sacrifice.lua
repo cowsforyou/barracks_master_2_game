92071f6c15137e675984313fdfd4a5ff48b7b1ba
cowsforyou <myrontayjt@gmail.com>
2020-11-04 20:43:21 +0800

    Finalise for xoo_rage_sacrifice

--- scripts/vscripts/spells/xoo_rage_sacrifice.lua
+++ scripts/vscripts/spells/xoo_rage_sacrifice.lua
@@ -3,7 +3,7 @@ xoo_rage_sacrifice = class({})
 --------------------------------------------------------------------------------
 function Sacrifice(keys)
 	local ability = keys.ability
-	local new_hp = keys.target:GetHealth() * ability:GetSpecialValueFor("sacrifice_hp_damage_pct")
+	local new_hp = keys.target:GetHealth() * (1-ability:GetSpecialValueFor("health_loss_pct"))
 	
 	if new_hp < 1 then  --Cannot kill the caster from its HP drain.
 		new_hp = 1
