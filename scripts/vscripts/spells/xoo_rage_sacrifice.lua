6278c923fdec49c9958fb7997cb415a553fd817e
Eugene Soo <20952179+eugenesoo@users.noreply.github.com>
2020-11-04 20:29:33 +0800

    Put sacrifice pct in datadriven file
    Signed-off-by: Eugene Soo <20952179+eugenesoo@users.noreply.github.com>

--- scripts/vscripts/spells/xoo_rage_sacrifice.lua
+++ scripts/vscripts/spells/xoo_rage_sacrifice.lua
@@ -2,7 +2,8 @@ xoo_rage_sacrifice = class({})
 
 --------------------------------------------------------------------------------
 function Sacrifice(keys)
-	local new_hp = keys.target:GetHealth() * 0.8
+	local ability = keys.ability
+	local new_hp = keys.target:GetHealth() * ability:GetSpecialValueFor("sacrifice_hp_damage_pct")
 	
 	if new_hp < 1 then  --Cannot kill the caster from its HP drain.
 		new_hp = 1
